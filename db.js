e37f0cbc0b1a69a494226e0a525bfcb0c9d73729
Toni Singh <toni.patna525@gmail.com>
2024-11-25 14:06:29 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //Define the MongoDB connection URL
-//const mongoURL ='mongodb://127.0.0.1:27017/hotels'  
+//const mongoURL = process.env.MONGODB_URL_LOCAL  
 const mongoURL = process.env.MONGODB_URL;
 
 //Set up MongoDB connection
